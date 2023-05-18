11bfda6039efc046804646a8eb922dbbb842a106
AquaKloud <133627006+AquaKloud@users.noreply.github.com>
2023-05-18 22:32:14 +0530

    Update Example.java

--- 05) Simple Data (Java Literals) 	Integer, Floating-point, Character, Boolean and String/Example.java
+++ 05) Simple Data (Java Literals) 	Integer, Floating-point, Character, Boolean and String/Example.java
@@ -1,1 +1,37 @@
+public class Example {
+    public static void main(String []args) {
+        System.out.println("Niroth");
+        System.out.println(10023);
+        System.out.println(12.34);
+    }
+}
+
+public class Example {
+    public static void main(String []args) {
+        System.out.println(1234); //Integer Literals
+        System.out.println(-1234); //Integer Literals
+        
+        System.out.println(1.234); //Floating-point Literals
+        System.out.println(0.0012); //Floating-point Literals
+        System.out.println(-0.0012); //Floating-point Literals
+        
+        System.out.println('A'); //Character Literals
+        System.out.println('7'); //Character Literals
+        
+        System.out.println(true); //Boolean Literals
+        System.out.println(false); //Boolean Literals
+        
+        System.out.println("Niroth"); //String Literals
+        System.out.println("A"); //String Literals
+        System.out.println("7"); //String Literals
+    }
+}
+
+public class Example {
+    public static void main(String []args) {
+        System.out.println(7); //Integer
+        System.out.println('7');//Character
+        System.out.println("7");//String
+    }
+}
 
