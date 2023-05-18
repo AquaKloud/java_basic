11cfb626cbd74a730428128cb7db0e1be0e7e059
AquaKloud <133627006+AquaKloud@users.noreply.github.com>
2023-05-18 22:27:57 +0530

    Update Example.java

--- 02) Invalid main method/Example.java
+++ 02) Invalid main method/Example.java
@@ -1,1 +1,33 @@
+class Example{
+	public static void main(String args){
+		System.out.println("Hello Java");
+	}
+}
+class Example{
+	public static void main(){
+		System.out.println("Hello Java");
+	}
+}
+class Example{
+	public void main(String[] args){
+		System.out.println("Hello Java");
+	}
+}
 
+class Example{
+	static void main(String[] args){
+		System.out.println("Hello Java");
+	}
+}
+
+class Example{
+	public static void Main(String[] args){
+		System.out.println("Hello Java");
+	}
+}
+
+class Example{
+	public static void danapala(String[] args){
+		System.out.println("Hello Java");
+	}
+}
