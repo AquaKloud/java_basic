c3479c87b6fe9f57d8ee1ede173bd12e8e5abcc5
AquaKloud <133627006+AquaKloud@users.noreply.github.com>
2023-05-18 21:05:10 +0530

    Update Example.java

--- 01) Valid main method declarations/Example.java
+++ 01) Valid main method declarations/Example.java
@@ -1,1 +1,25 @@
-
+class Example{
+	public static void main(String[] args){
+		System.out.println("Hello Java");
+	}
+}
+class Example{
+	public static void main(String args[]){
+		System.out.println("Hello Java");
+	}
+}
+class Example{
+	static public void main(String args[]){
+		System.out.println("Hello Java");
+	}
+}
+class Example{
+	public static void main(String ... args){
+		System.out.println("Hello Java");
+	}
+}
+class Example{
+	public static void main(String[] danapala){
+		System.out.println("Hello Java");
+	}
+}
