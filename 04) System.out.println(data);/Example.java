89a4da525efb5c7be2376e689da719591d92b86c
AquaKloud <133627006+AquaKloud@users.noreply.github.com>
2023-05-18 22:30:23 +0530

    Update Example.java

--- 04) System.out.println(data);/Example.java
+++ 04) System.out.println(data);/Example.java
@@ -1,1 +1,31 @@
+class Example{
+	public static void main(String args[]){
+		System.out.println("Hello Java");
+		System.out.println("Hello Java");
+		System.out.println("Hello Java");
+		System.out.println("Hello Java");
+		System.out.println("Hello Java");
+	
+	}
+}
+
+class Example{
+	public static void main(String args[]){
+		System.out.println("A");
+		System.out.println("B");
+		System.out.println("C");
+		System.out.println("D");
+		System.out.println("E");
+	}
+}
+
+class Example{
+	public static void main(String args[]){
+		System.out.println("A");
+		System.out.print("B");
+		System.out.println("C");
+		System.out.print("D");
+		System.out.println("E");
+	}
+}
 
