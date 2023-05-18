488a5dd412f49b806ab597717788ead7efeb0de1
AquaKloud <133627006+AquaKloud@users.noreply.github.com>
2023-05-18 22:28:23 +0530

    Update Example.java

--- 03) Illegal main methods/Example.java
+++ 03) Illegal main methods/Example.java
@@ -1,1 +1,17 @@
+class Example{
+	void public static  main(String[] args){
+		System.out.println("Hello Java");
+	}
+}
 
+class Example{
+	public static void main(String[] ){
+		System.out.println("Hello Java");
+	}
+}
+
+class Example{
+	public static void main(String[] args)
+		System.out.println("Hello Java");
+	}
+}
