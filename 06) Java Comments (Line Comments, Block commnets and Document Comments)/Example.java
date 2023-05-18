bc2cea0ab39253bb488432f4e55bb01da95393a7
AquaKloud <133627006+AquaKloud@users.noreply.github.com>
2023-05-18 22:33:49 +0530

    Update Example.java

--- 06) Java Comments (Line Comments, Block commnets and Document Comments)/Example.java
+++ 06) Java Comments (Line Comments, Block commnets and Document Comments)/Example.java
@@ -1,1 +1,22 @@
+//Line Comments
+public class Example {
+    public static void main(String []args) {
+		System.out.println("A"); //prints "A"
+		//System.out.println("B");
+		System.out.println("C");
+		//System.out.println("D");
+		System.out.println("E");
+    }
+}
+
+//Block Comments
+public class Example {
+    public static void main(String []args) {
+		System.out.println("A"); 
+		/*System.out.println("B");
+		System.out.println("C");
+		System.out.println("D");*/
+		System.out.println("E");
+    }
+}
 
