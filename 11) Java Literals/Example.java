b63c208ec7fdf1f95026d460d6c906d2b0ea6eab
AquaKloud <133627006+AquaKloud@users.noreply.github.com>
2023-05-19 10:03:19 +0530

    Update Example.java

--- 11) Java Literals/Example.java
+++ 11) Java Literals/Example.java
@@ -1,1 +1,206 @@
+class Example {
+    public static void main(String[] args) {
+        System.out.println(100); //prints 100
+        System.out.println(12345); 
+        System.out.println(-12345); 
+    }
+}
 
+
+class Example {
+    public static void main(String[] args) {
+        System.out.println(100); //Decimal Literals
+        System.out.println(0B1100100); //Binary Literals
+        System.out.println(0144); //Octal Literals
+        System.out.println(0X64); //Hexadecimal
+    }
+}
+
+
+class Example {
+
+    public static void main(String[] args) {
+        System.out.println(100); //Decimal
+        System.out.println(0B1100100); //Binary        
+        System.out.println(0B112); //Illegal ?
+    }
+}
+
+
+class Example {
+
+    public static void main(String[] args) {
+        System.out.println(100); //Decimal
+        System.out.println(0144); //Ocatal
+        System.out.println(0147); //
+        System.out.println(0148); //Illegal ?
+    }
+}
+
+
+class Example {
+
+    public static void main(String[] args) {
+        System.out.println(100); //Decimal
+        System.out.println(0x64); //Hexadecimal '0x' or '0X'
+        System.out.println(0X64);
+        System.out.println(0Xabcd);
+        System.out.println(0XABCD);
+    }
+}
+
+
+class Example {
+    public static void main(String[] args) {
+        System.out.println(0XABCDEF);
+        System.out.println(0XABCDG);
+    }
+}
+
+
+class Example {
+    public static void main(String []args) {
+		System.out.println(Integer.MAX_VALUE==0x7fffffff);
+	}
+}
+
+
+class Example {
+
+    public static void main(String[] args) {
+        System.out.println(1000000000.0);
+        System.out.println(1E9);
+        System.out.println(1E3);
+        System.out.println(0.00012);
+        System.out.println(1.2E-4);
+        System.out.println(-1.2E-4);
+    }
+}
+
+
+class Example {
+
+    public static void main(String[] args) {
+        System.out.println('A');
+        System.out.println('7'); //character 7
+        System.out.println('*');
+    }
+}
+
+
+class Example {
+
+    public static void main(String[] args) {
+        System.out.println('AB'); //Illegal
+    }
+}
+
+
+class Example {
+
+    public static void main(String[] args) {
+        System.out.println('\b'); //backspace character
+        System.out.println('\n'); //newline character
+        System.out.println('\t'); //backspace character
+        System.out.println('\f'); //form feed
+        System.out.println('\r'); //carriage return character
+        System.out.println('\''); //single quote
+        System.out.println('\"'); //double quoto
+        System.out.println('\\'); //backslash character
+    }
+}
+
+
+
+class Example {
+
+    public static void main(String[] args) {
+        System.out.println('\g');
+    }
+}
+
+
+class Example {
+
+    public static void main(String[] args) {
+        System.out.println("AB\tCD");
+        System.out.println("AB\bCD");
+        System.out.println("AB\nCD");
+    }
+}
+
+
+class Example {
+
+    public static void main(String[] args) {
+        System.out.println("AB\rCD");
+        System.out.println("AB\fCD");
+        System.out.println("AB\'CD");
+    }
+}
+
+
+class Example {
+
+    public static void main(String[] args) {
+        System.out.println("AB\\CD");
+        System.out.println("AB\'CD");
+        System.out.println("AB\"CD");
+    }
+}
+
+
+
+class Example {
+
+    public static void main(String[] args) {
+        System.out.println("/\/\/\"); //output-->/\/\/\
+    }
+}
+
+                           
+class Example {
+
+    public static void main(String[] args) {
+        System.out.println(); //output-->C:\\Windows\"Calc.exe"
+    }
+}
+                           
+
+class Example {
+    public static void main(String []args) {
+        System.out.println("C:\\\\Windows\\\"Calc.exe\""); //output-->C:\\Windows\"Calc.exe"
+	}
+}                           
+                           
+                           
+
+class Example {
+    public static void main(String []args) {
+        System.out.println('\u0041'); //prints A
+        System.out.println('\u0042'); //prints B
+	}
+}
+
+
+
+class Example {
+    public static void main(String []args) {
+		char \u0061='\u0061';
+        System.out.println(a); //prints a
+	}
+}
+
+                           
+class Example {
+    public static void main(String[] args) {
+        System.out.println(7); //Integer
+        System.out.println(7.0f); //float
+        System.out.println(7.0); //double
+        System.out.println('7'); //character
+        System.out.println("7"); //String
+    }
+}
+                           
+                           
+                           
