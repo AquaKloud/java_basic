7575d463ecb91a21cfc0e03014b4d7bf04ccb128
AquaKloud <133627006+AquaKloud@users.noreply.github.com>
2023-05-19 09:46:53 +0530

    Update Example.java

--- Keyboard Input/Example.java
+++ Keyboard Input/Example.java
@@ -1,1 +1,159 @@
+import java.util.*;
+class Example {
+    public static void main(String []args) {
+		Scanner input=new Scanner(System.in);
+		int x,y,z;
+		x=input.nextInt();
+		y=input.nextInt();
+		z=x+y;
+		System.out.println(x+" + "+y+" = "+z); //10 + 20 = 30
+	}
+}
+
+import java.util.*;
+class Example {
+    public static void main(String []args) {
+		Scanner input=new Scanner(System.in);
+		int x,y,z;
+		x=input.nextInt();
+		y=input.nextInt();
+		System.out.println("Input num1  : ");
+		System.out.println("Input num2  : ");
+		z=x+y;
+		System.out.println(x+" + "+y+" = "+z); 
+	}
+}
+
+import java.util.*;
+class Example {
+    public static void main(String []args) {
+		Scanner input=new Scanner(System.in);
+		int x,y,z;
+		System.out.println("Input num1  : ");
+		System.out.println("Input num2  : ");
+		x=input.nextInt();
+		y=input.nextInt();
+		z=x+y;
+		System.out.println(x+" + "+y+" = "+z); 
+	}
+}
+
+import java.util.*;
+class Example {
+    public static void main(String []args) {
+		Scanner input=new Scanner(System.in);
+		int x,y,z;
+		System.out.println("Input num1  : ");
+		x=input.nextInt();
+		System.out.println("Input num2  : ");
+		y=input.nextInt();
+		z=x+y;
+		System.out.println(x+" + "+y+" = "+z); 
+	}
+}
+
+
+import java.util.*;
+class Example {
+    public static void main(String []args) {
+		Scanner input=new Scanner(System.in);
+		int x,y,z;
+		System.out.print("Input num1  : ");
+		x=input.nextInt();
+		System.out.print("Input num2  : ");
+		y=input.nextInt();
+		z=x+y;
+		System.out.println(x+" + "+y+" = "+z); 
+	}
+}
+
+
+import java.util.*;
+class Example {
+    public static void main(String []args) {
+		Scanner input=new Scanner(System.in);
+		System.out.print("Input your age : ");
+		int age=input.nextInt();
+		System.out.println("Current age : "+age);
+		
+		//find your age after 10 years
+		
+			
+		//output
+		System.out.println("Your age after 10 years : "+age);
+	}
+}
+
+import java.util.*;
+class Example {
+    public static void main(String []args) {
+		Scanner input=new Scanner(System.in);
+		System.out.print("Input your age : ");
+		int age=input.nextInt();
+		System.out.println("Current age : "+age);
+		
+		//find your age after 10 years
+		age=age+10;
+			
+		//output
+		System.out.println("Your age after 10 years : "+age);
+	}
+}
+
+import java.util.*;
+class Example {
+    public static void main(String []args) {
+		Scanner input=new Scanner(System.in);
+		int x,y;
+		System.out.print("Input num1  : ");
+		x=input.nextInt();
+		System.out.print("Input num2  : ");
+		y=input.nextInt();
+		System.out.println(x+"  "+y); //12 13
+		//
+		//Insert code
+		//
+		System.out.println(x+"  "+y);//13 12
+	}
+}
+
+import java.util.*;
+class Example {
+    public static void main(String []args) {
+		Scanner input=new Scanner(System.in);
+		int x,y;
+		System.out.print("Input num1  : ");
+		x=input.nextInt();
+		System.out.print("Input num2  : ");
+		y=input.nextInt();
+		System.out.println(x+"  "+y); //12 13
+		//
+		int temp;
+		temp=x;
+		x=y;
+		y=temp;
+		//
+		System.out.println(x+"  "+y);//13 12
+	}
+}
+
+import java.util.*;
+class Example {
+    public static void main(String []args) {
+		Scanner input=new Scanner(System.in);
+		int x,y;
+		System.out.print("Input num1  : ");
+		x=input.nextInt();
+		System.out.print("Input num2  : ");
+		y=input.nextInt();
+		System.out.println(x+"  "+y); //12 13
+		//
+		x=x+y;
+		y=x-y;
+		x=x-y;
+		//
+		System.out.println(x+"  "+y);//13 12
+	}
+}
+
 
