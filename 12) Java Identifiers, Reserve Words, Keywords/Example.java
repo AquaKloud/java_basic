c599f7a12eb5fb7cbfdb6db2851a7175be4d8d2c
AquaKloud <133627006+AquaKloud@users.noreply.github.com>
2023-05-19 10:06:31 +0530

    Update Example.java

--- 12) Java Identifiers, Reserve Words, Keywords/Example.java
+++ 12) Java Identifiers, Reserve Words, Keywords/Example.java
@@ -1,1 +1,45 @@
+class Example{
+	public static void main(String args[]){
+		int customerTotal; //LEgal
+		int customer total; //Illeal
+	}
+}
+
+class Example{
+	public static void main(String args[]){
+		int age, my_age;
+		int $count;
+		int age#123; //Illegal
+	}
+}
+
+
+class Example{
+	public static void main(String args[]){
+		int aaaaaaaaaaaaaaaaaaaaaaaa; //Legal
+		int fdssdsdfsdsdfsdfsdfdsfsdf; //Legal
+	}
+}
+
+
+class Example{
+	public static void main(String args[]){
+		int For,Class, Int ;//Legal
+		int for; //Illegl
+		int byte; //Illegal
+	}
+}
+
+
+class Example{
+	public static void main(String args[]){
+		int count2, x1,x2; //Legal
+		
+		int 2count;  //Illegal
+		int 2x;	//Illegal
+	
+	}
+}
+
+
 
