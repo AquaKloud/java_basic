e9049a4d1f3140bcb54589a95bdd6bc454f1a4f0
AquaKloud <133627006+AquaKloud@users.noreply.github.com>
2023-05-29 22:45:36 +0530

    Update Example.java

--- 16) Java Flow Control "if else"/Example.java
+++ 16) Java Flow Control "if else"/Example.java
@@ -1,1 +1,41 @@
+import java.util.*;
+class Example {
+    public static void main(String []args) {
+		Scanner input=new Scanner(System.in);
+		System.out.print("Input average marks : ");
+		double avg =input.nextDouble();
+		System.out.println("Pass");
+		System.out.println("Fail");
+	}
+}
+
+import java.util.*;
+class Example {
+    public static void main(String []args) {
+		Scanner input=new Scanner(System.in);
+		System.out.print("Input average marks : ");
+		double avg =input.nextDouble();
+		if(avg>=50){
+			System.out.println("Pass");
+		}
+		if(avg<50){
+			System.out.println("Fail");
+		}
+	}
+}
+
+
+import java.util.*;
+class Example {
+    public static void main(String []args) {
+		Scanner input=new Scanner(System.in);
+		System.out.print("Input average marks : ");
+		double avg =input.nextDouble();
+		if(avg>=50){
+			System.out.println("Pass");
+		}else{
+			System.out.println("Fail");
+		}
+	}
+}
 
