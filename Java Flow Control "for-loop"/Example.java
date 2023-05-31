d087cd0b692851756bdba2fe2ec976e7e3d9d822
AquaKloud <133627006+AquaKloud@users.noreply.github.com>
2023-05-31 21:09:39 +0530

    Update Example.java

--- Java Flow Control "for-loop"/Example.java
+++ Java Flow Control "for-loop"/Example.java
@@ -1,1 +1,237 @@
+import java.util.*;
+class Example{
+	public static void main(String args[]){
+		System.out.println("Hello Java");	
+		System.out.println("Hello Java");	
+		System.out.println("Hello Java");	
+		System.out.println("Hello Java");	
+		System.out.println("Hello Java");	
+		System.out.println("Hello Java");	
+		System.out.println("Hello Java");	
+		System.out.println("Hello Java");	
+		System.out.println("Hello Java");	
+		System.out.println("Hello Java");	
+	}	
+}
+
+
+import java.util.*;
+class Example{
+	public static void main(String args[]){
+		for(int i=0; i<10; i++) System.out.println("Hello Java");	
+		/*System.out.println("Hello Java");	
+		System.out.println("Hello Java");	
+		System.out.println("Hello Java");	
+		System.out.println("Hello Java");	
+		System.out.println("Hello Java");	
+		System.out.println("Hello Java");	
+		System.out.println("Hello Java");	
+		System.out.println("Hello Java");	
+		System.out.println("Hello Java");*/
+	}	
+}
+
+
+import java.util.*;
+class Example{
+	public static void main(String args[]){
+		System.out.println("Start");	
+		for(int i=0; i<10; i++) System.out.println("Hello Java");	
+		System.out.println("End.");	
+	}	
+}
+
+
+import java.util.*;
+class Example{
+	public static void main(String args[]){
+		System.out.println("Start");	
+		for(int i=0; i<10; i++) 
+			System.out.println("Hello Java");	
+		System.out.println("End.");	
+	}	
+}
+
+
+
+import java.util.*;
+class Example{
+	public static void main(String args[]){
+		System.out.println("Start");
+		for(int i=0; i<10; i++) 
+			System.out.println("A");
+			System.out.println("B");
+		System.out.println("End.");	
+	}	
+}
+
+
+import java.util.*;
+class Example{
+	public static void main(String args[]){
+		System.out.println("Start");
+		for(int i=0; i<10; i++){ 
+			System.out.println("A");
+			System.out.println("B");
+		}
+		System.out.println("End.");	
+	}	
+}
+
+
+import java.util.*;
+class Example{
+	public static void main(String args[]){
+		System.out.println("Start");
+		for(int i=0; i<10; i++){ 
+			System.out.println("Hello Java");
+		}
+		System.out.println("End.");	
+	}	
+}
+
+
+import java.util.*;
+class Example{
+	public static void main(String args[]){
+		System.out.println("Start");
+		for(int i=0; i<5; i++){ 
+			System.out.println("Hello Java");
+		}
+		System.out.println("End.");	
+	}	
+}
+
+
+import java.util.*;
+class Example{
+	public static void main(String args[]){
+		System.out.println("Start");
+		for(int i=5; i<10; i++){ 
+			System.out.println("Hello Java");
+		}
+		System.out.println("End.");	
+	}	
+}
+
+
+
+import java.util.*;
+class Example{
+	public static void main(String args[]){
+		System.out.println("Start");
+		for(int i=0; i<10; i+=2){ 
+			System.out.println("Hello Java");
+		}
+		System.out.println("End.");	
+	}	
+}
+
+
+import java.util.*;
+class Example{
+	public static void main(String args[]){
+		Scanner input=new Scanner(System.in);
+		System.out.print("Input a number ");
+		int x0=input.nextInt();
+		System.out.print("Input a number ");
+		int x1=input.nextInt();
+		System.out.print("Input a number ");
+		int x2=input.nextInt();
+		System.out.print("Input a number ");
+		int x3=input.nextInt();
+		System.out.print("Input a number ");
+		int x4=input.nextInt();
+		System.out.print("Input a number ");
+		int x5=input.nextInt();
+		System.out.print("Input a number ");
+		int x6=input.nextInt();
+		System.out.print("Input a number ");
+		int x7=input.nextInt();
+		System.out.print("Input a number ");
+		int x8=input.nextInt();
+		System.out.print("Input a number ");
+		int x9=input.nextInt();
+		
+		//find total
+		int total=x0+x1+x2+x3+x4+x5+x6+x7+x8+x9;	
+		System.out.println("Total : "+total);	
+	}	
+}
+
+
+import java.util.*;
+class Example{
+	public static void main(String args[]){
+		Scanner input=new Scanner(System.in);
+		
+		int total=0;
+		int num;
+		
+		System.out.print("Input a number ");
+		num=input.nextInt();
+		total=total+num;
+		
+		System.out.print("Input a number ");
+		num=input.nextInt();
+		total=total+num;
+		
+		System.out.print("Input a number ");
+		num=input.nextInt();
+		total=total+num;
+		
+		System.out.print("Input a number ");
+		num=input.nextInt();
+		total=total+num;
+		
+		System.out.print("Input a number ");
+		num=input.nextInt();
+		total=total+num;
+		
+		System.out.print("Input a number ");
+		num=input.nextInt();
+		total=total+num;
+		
+		System.out.print("Input a number ");
+		num=input.nextInt();
+		total=total+num;
+		
+		System.out.print("Input a number ");
+		num=input.nextInt();
+		total=total+num;
+		
+		System.out.print("Input a number ");
+		num=input.nextInt();
+		total=total+num;
+		
+		System.out.print("Input a number ");
+		num=input.nextInt();
+		total=total+num;
+		
+		System.out.print("Input a number ");
+		num=input.nextInt();
+		total=total+num;
+		
+		System.out.println("Total : "+total);	
+	}	
+}
+
+
+import java.util.*;
+class Example{
+	public static void main(String args[]){
+		Scanner input=new Scanner(System.in);
+		
+		int total=0;
+		int num;
+		
+		for (int i = 0; i < 10; i++){
+			System.out.print("Input a number ");
+			num=input.nextInt();
+			total=total+num;
+		}
+		System.out.println("Total : "+total);	
+	}	
+}
+
 
