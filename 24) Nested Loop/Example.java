0666da33f4835976dc701e151c6fc3dfa5ca4273
AquaKloud <133627006+AquaKloud@users.noreply.github.com>
2023-06-01 21:45:55 +0530

    Update Example.java

--- 24) Nested Loop/Example.java
+++ 24) Nested Loop/Example.java
@@ -1,1 +1,49 @@
 
+import java.util.*;
+class Example{
+	public static void main(String args[]){
+		for(int i=0; i<10; i++){
+			for(int j=0; j<10; j++){
+				System.out.println(i+" "+j);
+			}
+		}
+	}	
+}
+
+import java.util.*;
+class Example{
+	public static void main(String args[]){
+		for(int i=0; i<10; i++){
+			for(int j=0; j<i+1; j++){
+				System.out.println(i+" "+j);
+			}
+			System.out.println("====");
+		}
+	}	
+}
+
+
+import java.util.*;
+class Example{
+	public static void main(String args[]){
+		for(int i=9; i>=0; i--){
+			for(int j=0; j<i+1; j++){
+				System.out.println(i+" "+j);
+			}
+			System.out.println("====");
+		}
+	}	
+}
+
+
+import java.util.*;
+class Example{
+	public static void main(String args[]){
+		for(int i=0; i<10; i++){
+			for(int j=0; j<10; j++){
+				System.out.print("*  ");
+			}
+			System.out.println();
+		}
+	}	
+}
